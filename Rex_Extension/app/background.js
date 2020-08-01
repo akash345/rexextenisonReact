17912c13f41741006592c8529182029e19cb686b
Vikram Ruppa-Kasani <vikram@Vikrams-MacBook-Pro.local>
2020-08-01 14:53:08 -0400

    Added Firebase Support

--- Rex_Extension/app/background.js
+++ Rex_Extension/app/background.js
@@ -1,6 +1,24 @@
 /*global chrome*/
 console.log("We are receiving");
 alert("hello");
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Other config options...
+});
+
+//TODO(Vikram) Populate these values from frontend user submitted data
+var userName = "vikramrk"
+var email = "vikramrk3@gmail.com"
+var phoneNumber = 6789069312
+var contactNames = ["Rao"];
+var contactNum = [1112223333];
+
 function getURL() {
 console.log('Where do you want to send this url:');
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>  {
