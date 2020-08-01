6fd5d78a407e05e0fc95ac79b8eb5c1131d12b9f
Vikram Ruppa-Kasani <vikram@Vikrams-MacBook-Pro.local>
2020-08-01 16:44:37 -0400

    stuff

--- Rex_Extension/app/background.js
+++ Rex_Extension/app/background.js
@@ -1,7 +1,7 @@
 /*global chrome*/
 console.log("We are receiving");
 alert("hello");
-
+/*
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
@@ -10,14 +10,29 @@ ui.start('#firebaseui-auth-container', {
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   // Other config options...
-});
+}); */
+
+
 
 //TODO(Vikram) Populate these values from frontend user submitted data
 var userName = "vikramrk"
 var email = "vikramrk3@gmail.com"
+var password = "rex"
 var phoneNumber = 6789069312
 var contactNames = ["Rao"];
 var contactNum = [1112223333];
+
+fetch('/__/firebase/init.json').then(async response => {
+  firebase.initializeApp(await response.json());
+});
+
+
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
 
 function getURL() {
 console.log('Where do you want to send this url:');
