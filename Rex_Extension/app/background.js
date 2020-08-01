51e195ece05832e25311ac0a581e5e6291e226d0
Vikram Ruppa-Kasani <vikram@Vikrams-MacBook-Pro.local>
2020-08-01 14:20:47 -0400

    Added_Firebase_HTML

--- Rex_Extension/app/background.js
+++ Rex_Extension/app/background.js
@@ -1,4 +1,6 @@
 /*global chrome*/
+console.log("We are receiving");
+alert("hello");
 function getURL() {
 console.log('Where do you want to send this url:');
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs =>  {
